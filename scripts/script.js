9c17ef35ff0f77fbd962cca011326162232a2a0e
Henri Herrera <henriherrera18@gmail.com>
2020-01-28 11:13:50 -0400

    Addig script file

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,3 @@
 var button = document.getElementByClassName(w3-button);
 
-button.onclick="location.href='https://xwolfx.github.io/Paint/love'";
+button.onclick="location.href='https://xwolfx.github.io/Paint/love';"
